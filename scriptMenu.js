ead13f0822ade97f92f229018092fbc9a1d881a3
Fabio Costa <fabiocos996@gmail.com>
2023-09-28 18:08:01 -0300

    Estrutura do site

---
+++ scriptMenu.js
@@ -0,0 +1,22 @@
+openMenu.addEventListener('click', () => {
+    menu.style.display="flex"
+    
+    menu.style.right=(menu.offsetWidth * -1)+'px'
+    setTimeout(()=> {
+        menu.style.opacity= '1'
+        menu.style.right= "0"
+        openMenu.style.display= 'none'
+    }, 10);
+})
+
+closeMenu.addEventListener('click',()=>{
+    menu.style.opacity="0"
+    menu.style.right=(menu.offsetWidth*-1)+'px'
+    
+    setTimeout(()=>{
+        menu.removeAtribute("style")
+        openMenu.removeAtribute('style')
+        
+    },200);
+    
+})
